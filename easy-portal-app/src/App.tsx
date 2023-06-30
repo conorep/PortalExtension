5fbb4fed6854f2e4f2f57a25bd81ba24361d4197
Conor O'Brien <Conor.O'Brien@astronics.com>
2023-06-29 18:13:58 -0700

    initial extension build commit - so far, just have a basic extension with a 'hello world' string output to browser    - base layer of app is put together, so now comes the fun part

---
+++ easy-portal-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App(){
+    return (
+        <>
+            <header>
+                <h5 id={'styledHeader5'}>Hello from EasyPortal app!</h5>
+            </header>
+        </>
+    )
+}
