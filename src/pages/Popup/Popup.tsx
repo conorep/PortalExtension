8c65a2f6efb2e7ceba0f6e3342f3c84de51d9ccf
Conor O'Brien <Conor.O'Brien@astronics.com>
2023-07-13 16:38:06 -0700

    more boilerplate code

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Popup.css';
+const logo = require("'../../assets/img/logo.svg") as string;
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
